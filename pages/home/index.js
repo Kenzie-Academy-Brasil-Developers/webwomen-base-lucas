e1c009e2f4b93d0498fa1c841fee8bbdf96769e4
Baffi <lucasrenatobaffi@hotmail.com>
2022-10-09 01:07:40 -0300

    renderizacao dos cards e ajustes na estilizacao

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,51 @@
 /* Desenvolva sua lógica aqui... */
+function createCategoryButton(array) {
+    array.forEach((elt) => {
+      const button = document.querySelector(".home-office"); 
+     
+    if(button){
+        button.innerHTML = elt
+    }
+  
+     return button
+    });
+  }
+
+
+const createJob = (jobsData) => {
+    const {enterprise, title, location, descrition, modalities} = jobsData
+    let div = document.createElement('div')
+    div.classList.add('div-card')
+  
+    div.insertAdjacentHTML("afterbegin", `
+      
+    <h3>${title}</h3>
+    <div class="div-info">
+      <p>${enterprise}</p>
+      <p>${location}</p>
+    </div>
+    <p class="paragraph-lorem">${descrition}
+      </p>
+      <div class="div-button">
+        <button class="home-office"${createCategoryButton(modalities)}</button>
+      <button class="candidatar">Candidatar</button>
+      
+      `)
+ 
+    return div
+  }
+
+const section = document.querySelector(".section-vagas")
+const renderJob = (cardList) => {
+
+  section.innerHTML = ""
+
+  cardList.forEach((element) => {
+    const card = createJob(element)
+    section.appendChild(card)
+  })
+
+//   createJob(cardList)
+
+}
+renderJob(jobsData)
